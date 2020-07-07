76c215c86875f0fe4bb2a706679ea602de86068c
alvaralmstedt <alvar.almstedt@gmail.com>
2020-07-07 16:25:11 +0200

    more conda integration, removed some commented code

--- run_NPM1_analysis.sh
+++ run_NPM1_analysis.sh
@@ -2,71 +2,57 @@
 
 #echo "$HOSTNAME"
 
-scriptdir=$(dirname $0)
+scriptdir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-PYTHON=$(which python)
-#script1=/apps/bio/software/NPM1/python/main/pear.py
-#script2=/apps/bio/software/NPM1/python/main/pipeline_amplicon_pear.py
-#script3=/apps/bio/software/NPM1/python/main/npm_var.py
+echo "SCRIPTDIR: $scriptdir"
+
+echo "Establishing conda environment at `date`"
+conda env create -f $scriptdir/environment.yaml 2> /dev/null
+echo ".. Done at `date`"
+
+#echo "MY PYTHON: $(which python)"
+#PYTHON=$(which python)
 
 DATE=$(date +"%Y_%m_%d_%H_%M_%S")
-
-#module load PEAR/0.9.6
-#module load fastx/0.0.13
-#module load bwa/0.5.9
-#module load picard-tools/2.2.4
-#module load samtools/1.0
 
 FASTQDIR=$1
 OUTDIR=$2
 TMPDIR=$OUTDIR/tmp/NPM1_$DATE
-#SCRIPTS=/apps/bio/software/NPM1/python/main
-#KLINKEM_OUT=/seqstore/remote/share/klinkem01/Djupsekvensering/pipeline_outputs
 CSV_SAVELOC=$OUTDIR/csv
-#RUNNAME=$3
-EMAILS=$3
-
+REF=$3
+EMAILS=$4
 
 script1=$scriptdir/pear.py
 script2=$scriptdir/pipeline_amplicon_pear.py
 script3=$scriptdir/npm_var.py
 
-
 #Copies the fastq files to tmp directory to prepare to start working
-mkdir $TMPDIR
+mkdir -p $TMPDIR
 echo "Copying $FASTQDIR/*.fastq.gz to $TMPDIR"
-cp $FASTQDIR/*.fastq.gz $TMPDIR
+for file in $(ls $FASTQDIR/*.fastq.gz) ; do
+    cp $file $TMPDIR
+done
+
 ls $TMPDIR/*.fastq.gz > $TMPDIR/files.txt
 cd $TMPDIR
 
 #Gunzips and runs PEAR on the samples
 echo "Running: $PYTHON $script1 $TMPDIR/files.txt"
-$PYTHON $script1 $TMPDIR/files.txt
+conda run -n NPM1_DeepSeq $script1 $TMPDIR/files.txt
 
 #Does mapping etc
 echo "Running: $PYTHON $script2 $TMPDIR"
-$PYTHON $script2 $TMPDIR
-
+conda run -n NPM1_DeepSeq $script2 $TMPDIR $REF
 
 # bamfiles.txt is just a file that contains all the sorted bamfile from the previous step.
 # $FASTQDIR is used for providing context to the mail attachment in the for of run name
 # Email server etc. has to be edited in npm_var.py in order to be used. By default,
 # no emails will be sent if no adresses are given
 echo "Running: $PYTHON $script3 $TMPDIR/bamfiles.txt $FASTQDIR $EMAILS"
-$PYTHON $script3 $TMPDIR/bamfiles.txt $FASTQDIR $EMAILS
+conda run -n NPM1_DeepSeq $script3 $TMPDIR/bamfiles.txt $FASTQDIR $EMAILS
 
-#echo "Copying $TMPDIR/*.sorted.bam* to $KLINKEM_OUT/bam/$RUN"
-#cp $TMPDIR/*.sorted.bam* $KLINKEM_OUT/bam/$RUN
+#copy relevant csv file to .. from cwd
 
-#echo "Copying $TMPDIR/*.fastq.gz to $KLINKEM_OUT/fastq/$RUN"
-#cp $TMPDIR/*.fastq.gz $KLINKEM_OUT/fastq/$RUN
-
-
-#This would take all csvs from the analysis. For CLC import this is not needed at the moment
-#echo "Copying $TMPDIR/NPM1_*$(date '+%y%m%d').csv to $KLINKEM_OUT/csv ..."
-#cp $TMPDIR/NPM1_*$(date '+%y%m%d').csv $KLINKEM_OUT/csv
-#echo "Copying $TMPDIR/CLC_*.csv to $CSV_SAVELOC"
-#cp $TMPDIR/CLC_*.csv $CSV_SAVELOC
-
+#remove everything in tmpdir
 
 echo "Done at `date` !"
