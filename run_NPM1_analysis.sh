fabce90cf1a2b8c018e5925a39d6d0a540771847
alvaralmstedt <alvar.almstedt@gmail.com>
2020-07-14 11:57:06 +0200

    Trying the tsv output

--- run_NPM1_analysis.sh
+++ run_NPM1_analysis.sh
@@ -10,16 +10,11 @@ echo "Establishing conda environment at `date`"
 conda env create -f $scriptdir/environment.yaml 2> /dev/null
 echo ".. Done at `date`"
 
-#echo "MY PYTHON: $(which python)"
-#PYTHON=$(which python)
-
 DATE=$(date +"%Y_%m_%d_%H_%M_%S")
-DAY=$(date +"%Y%m%d")
 
 FASTQDIR=$1
 OUTDIR=$2
 TMPDIR=$OUTDIR/tmp/NPM1_$DATE
-CSV_SAVELOC=$OUTDIR/csv
 REF=$3
 EMAILS=$4
 
@@ -43,7 +38,7 @@ cd $TMPDIR
 echo "Running: $PYTHON $script1 $TMPDIR/files.txt"
 conda run -n NPM1_DeepSeq $script1 $TMPDIR/files.txt
 
-#Does mapping etc
+#Does QC, mapping etc
 echo "Running: $PYTHON $script2 $TMPDIR"
 conda run -n NPM1_DeepSeq $script2 $TMPDIR $REF
 
@@ -54,19 +49,20 @@ conda run -n NPM1_DeepSeq $script2 $TMPDIR $REF
 echo "Running: $PYTHON $script3 $TMPDIR/bamfiles.txt $FASTQDIR $EMAILS"
 conda run -n NPM1_DeepSeq $script3 $TMPDIR/bamfiles.txt $FASTQDIR $EMAILS
 
-#copy relevant csv file to .. from cwd
+DAY=$(date +"%y%m%d")
 
-echo "Copy loop"
-filestocopy=$(ls $TMPDIR | grep ".csv")
-for csv in $filestocopy ; do
-    echo "Copying $csv to $OUTIDIR"
-    cp $csv $OUTDIR
-done
+#Uncomment the following block to get all of the csv files
+#echo "Copy loop"
+#filestocopy=$(ls $TMPDIR | grep ".csv")
+#for csv in $filestocopy ; do
+#    echo "Copying $csv to $OUTIDIR"
+#    cp $csv $OUTDIR
+#done
 
-echo "Copying single file to outdir"
-cp $TMPDIR/NPM1_all_samples.csv $OUTDIR
+echo "Copying single csv file to outdir"
+cp $TMPDIR/NPM1_UDS_final_result.tsv $OUTDIR/NPM1_UDS_final_result_$(basename $FASTQDIR)_$DAY.tsv
 
-#remove everything in tmpdir
+#Remove everything in tmpdir, leaving only results.
 #rm -rf $TMPDIR
 
-echo "Done at `date` !"
+echo "Done at `date` ! Your output is located here: $OUTDIR/NPM1_$(basename $FASTQDIR)_$DAY.csv"
